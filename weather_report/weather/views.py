8bd7ff654e6157568085dce14faf16b789f63519
Sharvan Kotharu <ksharvan2007@gmail.com>
2026-05-31 17:31:17 +0530

    Update views.py

--- weather_report/weather/views.py
+++ weather_report/weather/views.py
@@ -15,7 +15,7 @@ def index(request):
             error_message = "Please enter a city name"
         else:
             try:
-                api_key = '//paste here'
+                api_key = '52526fd4c64e39df9b07c44e9de8bdd9'
                 url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
                 res = urllib.request.urlopen(url, timeout=5).read()
                 json_data = json.loads(res)
